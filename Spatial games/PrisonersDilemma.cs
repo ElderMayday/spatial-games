86e3a9e4c051e5b76c0d584dcc610c89d658d687
ElderMayday <galdarsar@gmail.com>
2016-11-09 18:32:51 +0100

    Add SymmetricGame and PrisonersDilemma

---
+++ Spatial games/PrisonersDilemma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spatial_games
+{
+    class PrisonersDilemma : SymmetricGame
+    {
+        public PrisonersDilemma(double Temptation, double Reward, double Punishment, double SuckersPayoff) : base(Reward, SuckersPayoff, Temptation, Punishment)
+        {
+        }
+    }
+}
