6416f73f491c112f7a0ac3c547b4fc73b8c9d105
ElderMayday <galdarsar@gmail.com>
2016-11-09 19:59:13 +0100

    Add lattices

---
+++ Spatial games/Lattice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spatial_games
+{
+    abstract class Lattice
+    {
+        public Lattice(int height, int width)
+        {
+            this.Height = height;
+            this.Width = width;
+
+            player = new Player[height, width];
+
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                    this.player[i, j] = new Player();
+        }
+
+        public Player[,] player { get; protected set; }
+        public int Height { get; protected set; }
+        public int Width { get; protected set; }
+
+        protected abstract List<Player> getNeighboursOfPlayer(int row, int column);
+    }
+}
---
+++ Spatial games/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spatial_games
+{
+    class Player
+    {
+        public Player()
+        {
+            this.ChosenAction = Generator.Next(2) == 0 ? Action.One : Action.Two;
+            this.Payoff = 0;
+        }
+
+        public Action ChosenAction { get; set; }
+        public double Payoff { get; set; }
+    }
+}
