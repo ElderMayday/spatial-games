8a50e474441134290a2804627df2926b4fb0fc86
ElderMayday <galdarsar@gmail.com>
2016-11-09 20:51:14 +0100

    Add neighbour determiners

---
+++ Spatial games/NeighbourDeterminer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spatial_games
+{
+    abstract class NeighbourDeterminer
+    {
+        public NeighbourDeterminer(Lattice lattice)
+        {
+            this.Lattice = lattice;
+        }
+
+        abstract public List<Player> Determine(int row, int column);
+
+        public Lattice Lattice { get; protected set; }
+    }
+}
---
+++ Spatial games/NeighbourDeterminerMoore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spatial_games
+{
+    class NeighbourDeterminerMoore : NeighbourDeterminer
+    {
+        public NeighbourDeterminerMoore(Lattice lattice) : base(lattice)
+        {
+        }
+
+        public override List<Player> Determine(int row, int column)
+        {
+            List<Player> result = new List<Player>();
+
+            int rowUp, rowDown, columnLeft, columnRight;
+
+            rowUp = row > 0 ? row - 1 : this.Lattice.Height - 1;
+            rowDown = row < this.Lattice.Height - 1 ? row + 1 : 0;
+            columnLeft = column > 0 ? column - 1 : this.Lattice.Width - 1;
+            columnRight = column < this.Lattice.Width - 1 ? column + 1 : 0;
+
+            result.Add(this.Lattice.player[rowUp, columnLeft]);
+            result.Add(this.Lattice.player[rowUp, column]);
+            result.Add(this.Lattice.player[rowUp, columnRight]);
+            result.Add(this.Lattice.player[row, columnLeft]);
+            result.Add(this.Lattice.player[row, columnRight]);
+            result.Add(this.Lattice.player[rowDown, columnLeft]);
+            result.Add(this.Lattice.player[rowDown, column]);
+            result.Add(this.Lattice.player[rowDown, columnRight]);
+
+            return result;
+        }
+    }
+}
