83e0d7fa62e584127573133d20eca87c5291e6ce
ElderMayday <galdarsar@gmail.com>
2016-11-09 19:51:18 +0100

    Add number generator

---
+++ Spatial games/Generator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spatial_games
+{
+    static class Generator
+    {
+        static private Random random = new Random();
+
+        static public int Next(int number)
+        {
+            return random.Next(number);
+        }
+    }
+}
