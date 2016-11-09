7be8791645b041493b8f19abae0a06569437e43e
ElderMayday <galdarsar@gmail.com>
2016-11-09 20:52:11 +0100

    Add ActionReselectors

---
+++ Spatial games/ActionReselector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spatial_games
+{
+    abstract class ActionReselector
+    {
+        public ActionReselector()
+        {
+        }
+
+        public abstract Action Reselect(List<Player> neighbours);
+    }
+}
---
+++ Spatial games/ActionReselectorMax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spatial_games
+{
+    class ActionReselectorMax : ActionReselector
+    {
+        public override Action Reselect(List<Player> neighbours)
+        {
+            double maxPayoff = neighbours.Max(x => x.Payoff);
+            Player bestNeighbour = neighbours.Find(x => x.Payoff == maxPayoff);
+            return bestNeighbour.ChosenAction;
+        }
+    }
+}
