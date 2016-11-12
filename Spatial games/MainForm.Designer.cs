3085b8efd3ff5f002ea905ad4aed846f542f3eca
ElderMayday <galdarsar@gmail.com>
2016-11-12 02:55:28 +0100

    Implement multiple experiments

--- Spatial games/MainForm.Designer.cs
+++ Spatial games/MainForm.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.picture = new System.Windows.Forms.PictureBox();
             this.buttonSimulate = new System.Windows.Forms.Button();
             this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
@@ -53,11 +53,16 @@
             this.radioButtonInstant = new System.Windows.Forms.RadioButton();
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPageLattice = new System.Windows.Forms.TabPage();
-            this.tabPagePlot = new System.Windows.Forms.TabPage();
-            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.buttonPlot = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPagePlot = new System.Windows.Forms.TabPage();
+            this.tabControlPlot = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.chartTime = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.buttonPlot = new System.Windows.Forms.Button();
+            this.numericUpDownSimulations = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).BeginInit();
@@ -69,7 +74,10 @@
             this.tabControl.SuspendLayout();
             this.tabPageLattice.SuspendLayout();
             this.tabPagePlot.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.tabControlPlot.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartTime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSimulations)).BeginInit();
             this.SuspendLayout();
             // 
             // picture
@@ -309,43 +317,16 @@
             this.tabPageLattice.Text = "Lattice";
             this.tabPageLattice.UseVisualStyleBackColor = true;
             // 
-            // tabPagePlot
+            // label2
             // 
-            this.tabPagePlot.Controls.Add(this.chart);
-            this.tabPagePlot.Controls.Add(this.buttonPlot);
-            this.tabPagePlot.Location = new System.Drawing.Point(4, 22);
-            this.tabPagePlot.Name = "tabPagePlot";
-            this.tabPagePlot.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPagePlot.Size = new System.Drawing.Size(1177, 560);
-            this.tabPagePlot.TabIndex = 1;
-            this.tabPagePlot.Text = "Plot";
-            this.tabPagePlot.UseVisualStyleBackColor = true;
-            // 
-            // chart
-            // 
-            chartArea3.Name = "ChartArea1";
-            this.chart.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart.Legends.Add(legend3);
-            this.chart.Location = new System.Drawing.Point(216, 6);
-            this.chart.Name = "chart";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.chart.Series.Add(series3);
-            this.chart.Size = new System.Drawing.Size(955, 508);
-            this.chart.TabIndex = 1;
-            this.chart.Text = "chart1";
-            // 
-            // buttonPlot
-            // 
-            this.buttonPlot.Location = new System.Drawing.Point(6, 6);
-            this.buttonPlot.Name = "buttonPlot";
-            this.buttonPlot.Size = new System.Drawing.Size(165, 48);
-            this.buttonPlot.TabIndex = 0;
-            this.buttonPlot.Text = "Plot";
-            this.buttonPlot.UseVisualStyleBackColor = true;
-            this.buttonPlot.Click += new System.EventHandler(this.buttonPlot_Click);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(345, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Yellow - Defect";
             // 
             // label1
             // 
@@ -358,22 +339,102 @@
             this.label1.TabIndex = 13;
             this.label1.Text = "Red - Cooperate";
             // 
-            // label2
+            // tabPagePlot
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.label2.Location = new System.Drawing.Point(345, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 16);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "Yellow - Defect";
+            this.tabPagePlot.Controls.Add(this.tabControlPlot);
+            this.tabPagePlot.Controls.Add(this.buttonPlot);
+            this.tabPagePlot.Location = new System.Drawing.Point(4, 22);
+            this.tabPagePlot.Name = "tabPagePlot";
+            this.tabPagePlot.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPagePlot.Size = new System.Drawing.Size(1177, 560);
+            this.tabPagePlot.TabIndex = 1;
+            this.tabPagePlot.Text = "Plot";
+            this.tabPagePlot.UseVisualStyleBackColor = true;
+            // 
+            // tabControlPlot
+            // 
+            this.tabControlPlot.Controls.Add(this.tabPage1);
+            this.tabControlPlot.Controls.Add(this.tabPage2);
+            this.tabControlPlot.Location = new System.Drawing.Point(190, 6);
+            this.tabControlPlot.Name = "tabControlPlot";
+            this.tabControlPlot.SelectedIndex = 0;
+            this.tabControlPlot.Size = new System.Drawing.Size(974, 548);
+            this.tabControlPlot.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chartTime);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(966, 522);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "FractionTime";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // chartTime
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartTime.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartTime.Legends.Add(legend1);
+            this.chartTime.Location = new System.Drawing.Point(6, 6);
+            this.chartTime.Name = "chartTime";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartTime.Series.Add(series1);
+            this.chartTime.Size = new System.Drawing.Size(955, 508);
+            this.chartTime.TabIndex = 1;
+            this.chartTime.Text = "chart1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(966, 522);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Fraction distribution";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // buttonPlot
+            // 
+            this.buttonPlot.Location = new System.Drawing.Point(6, 6);
+            this.buttonPlot.Name = "buttonPlot";
+            this.buttonPlot.Size = new System.Drawing.Size(165, 48);
+            this.buttonPlot.TabIndex = 0;
+            this.buttonPlot.Text = "Plot";
+            this.buttonPlot.UseVisualStyleBackColor = true;
+            this.buttonPlot.Click += new System.EventHandler(this.buttonPlot_Click);
+            // 
+            // numericUpDownSimulations
+            // 
+            this.numericUpDownSimulations.Location = new System.Drawing.Point(665, 12);
+            this.numericUpDownSimulations.Name = "numericUpDownSimulations";
+            this.numericUpDownSimulations.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDownSimulations.TabIndex = 14;
+            this.numericUpDownSimulations.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(597, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 31);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Number of simulations";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1221, 694);
+            this.Controls.Add(this.numericUpDownSimulations);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.tabControl);
             this.Controls.Add(this.groupBoxNeighbourhood);
             this.Controls.Add(this.groupBoxGame);
@@ -402,7 +463,10 @@
             this.tabPageLattice.ResumeLayout(false);
             this.tabPageLattice.PerformLayout();
             this.tabPagePlot.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.tabControlPlot.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartTime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSimulations)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -434,9 +498,14 @@
         private System.Windows.Forms.TabPage tabPageLattice;
         private System.Windows.Forms.TabPage tabPagePlot;
         private System.Windows.Forms.Button buttonPlot;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartTime;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControlPlot;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.NumericUpDown numericUpDownSimulations;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Spatial games/MainForm.cs
+++ Spatial games/MainForm.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using System.Linq;
 
 namespace Spatial_games
 {
@@ -75,7 +77,9 @@ namespace Spatial_games
 
         private void buttonPlot_Click(object sender, EventArgs e)
         {
-            chart.Series.Clear();
+            chartTime.Series.Clear();
+
+            int numberOfExperiments = (int)numericUpDownSimulations.Value;
 
             int numberOfRounds = (int)numericUpDownRounds.Value;
             int height = (int)numericUpDownHeight.Value;
@@ -84,24 +88,35 @@ namespace Spatial_games
             NeighbourhoodType neighbourhoodType = radioButtonMoore.Checked ? NeighbourhoodType.Moore : NeighbourhoodType.VonNeumann;
             Reselector reselector = radioButtonMax.Checked ? Reselector.Max : Reselector.Replicator;
 
-            Lattice lattice = new Lattice(height, width, neighbourhoodType, game, reselector);
+            List<double>[] fraction = new List<double>[numberOfRounds + 1];
 
-            chart.Series.Add("Cooperation");
-            chart.Series["Cooperation"].ChartType = SeriesChartType.FastLine;
-            chart.Series["Cooperation"].Color = Color.Red;
-            chart.Series["Cooperation"].BorderWidth = 3;
-            chart.ChartAreas[0].AxisY.Maximum = 1.0;
-            chart.ChartAreas[0].AxisY.Minimum = 0.0;
-            chart.ChartAreas[0].AxisX.Maximum = numberOfRounds;
-            chart.ChartAreas[0].AxisX.Minimum = 0.0;
+            for (int round = 0; round < numberOfRounds + 1; round++)
+                fraction[round] = new List<double>();
 
-            chart.Series["Cooperation"].Points.AddXY(0, lattice.RatioActionOne);
-
-            for (int i = 0; i < numberOfRounds; i++)
+            for (int experiment = 0; experiment < numberOfExperiments; experiment++)
             {
-                lattice.NextRound();
-                chart.Series["Cooperation"].Points.AddXY(i + 1, lattice.RatioActionOne);
+                Lattice lattice = new Lattice(height, width, neighbourhoodType, game, reselector);
+                
+                fraction[0].Add(lattice.RatioActionOne);
+
+                for (int round = 1; round < numberOfRounds + 1; round++)
+                {
+                    lattice.NextRound();
+                    fraction[round].Add(lattice.RatioActionOne);
+                }
             }
+
+            chartTime.Series.Add("Cooperation");
+            chartTime.Series["Cooperation"].ChartType = SeriesChartType.FastLine;
+            chartTime.Series["Cooperation"].Color = Color.Red;
+            chartTime.Series["Cooperation"].BorderWidth = 3;
+            chartTime.ChartAreas[0].AxisY.Maximum = 1.0;
+            chartTime.ChartAreas[0].AxisY.Minimum = 0.0;
+            chartTime.ChartAreas[0].AxisX.Maximum = numberOfRounds;
+            chartTime.ChartAreas[0].AxisX.Minimum = 0.0;
+
+            for (int round = 0; round < numberOfRounds + 1; round++)
+                chartTime.Series["Cooperation"].Points.AddXY(round, fraction[round].Sum() / (double)numberOfExperiments);
         }
     }
 }
