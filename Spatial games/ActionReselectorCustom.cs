3407c19efa2def2d9402d231b53ca45668a52354
ElderMayday <galdarsar@gmail.com>
2016-11-14 19:08:57 +0100

    Add ActionReselectorCustom

---
+++ Spatial games/ActionReselectorCustom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spatial_games
+{
+    public class ActionReselectorCustom : ActionReselector
+    {
+        public ActionReselectorCustom() : base()
+        {
+        }
+
+        public override Action Reselect(Player player, List<Player> neighbours)
+        {
+            List<Player> selected = neighbours.Where(x => x.Payoff > player.Payoff).ToList();
+
+            if (selected.Count == 0)
+                return player.ChosenAction;
+            else
+                return neighbours[Generator.Next(neighbours.Count)].ChosenAction;
+        }
+    }
+}
